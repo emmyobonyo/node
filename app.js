ed670e1e5dbd3bbe3e2445a8acf7a1f91e4d475f
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-03-15 08:16:27 +0300

    Learning node

---
+++ app.js
@@ -0,0 +1,7 @@
+const readline = require('readline');
+const rl = readline.createInteface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let num1 = Math.floor((Math.Random() * 10) + 1)
