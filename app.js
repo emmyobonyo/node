1e701477e3da8aeae06fc53ca5c73820b53e1609
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-03-15 09:00:54 +0300

    Delete created file

--- app.js
+++ app.js
@@ -13,3 +13,19 @@ fs.writeFile('example.txt', 'This is an example file', (err) => {
     })
   }
 })
+
+fs.appendFile('example.txt', 'Some data to append', (err)=>{
+  if(err){
+    console.log(err)
+  } else {
+    console.log('successfully appended data')
+  }
+})
+
+fs.unlink('example.txt', (err)=>{
+  if(err){
+    console.log(err)
+  } else {
+    console.log('File successfully deleted')
+  }
+})
