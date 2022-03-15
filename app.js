533fcd22b1c70dcc4713987e726b7ec7f8a5685a
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-03-15 08:49:43 +0300

    Read a file using an encoding system

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ fs.writeFile('example.txt', 'This is an example file', (err) => {
     console.log(err)
   } else {
     console.log('File successfully created')
+    fs.readFile('example.txt', 'utf8', (err, file)=> {
+      if(err){
+        console.log(err)
+      } else {
+        console.log(file)
+      }
+    })
   }
 })
