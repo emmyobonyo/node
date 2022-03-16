86d470cb8dff407253519aea5e3fe88b63de3ce9
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-03-16 15:53:36 +0300

    Reading a directory from a file

--- app.js
+++ app.js
@@ -1,31 +1,8 @@
-const fs = require('fs'); //file system module
-fs.writeFile('example.txt', 'This is an example file', (err) => {
-  if(err) {
+const fs = require('fs');
+fs.readdir('tutorial', (err,files) => {
+  if (err){
     console.log(err)
   } else {
-    console.log('File successfully created')
-    fs.readFile('example.txt', 'utf8', (err, file)=> {
-      if(err){
-        console.log(err)
-      } else {
-        console.log(file)
-      }
-    })
-  }
-})
-
-fs.appendFile('example.txt', 'Some data to append', (err)=>{
-  if(err){
-    console.log(err)
-  } else {
-    console.log('successfully appended data')
-  }
-})
-
-fs.unlink('example.txt', (err)=>{
-  if(err){
-    console.log(err)
-  } else {
-    console.log('File successfully deleted')
+    console.log(files)
   }
 })
