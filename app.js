24bcd38e36003f81a91f7504cc4b85f40d460fb9
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-03-16 16:33:56 +0300

    piping

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
-const readStream = fs.createReadStream('./exmaple.txt', 'utf8')
-const writeStream = fs.createWriteStream('exampleWrite.txt')
-readStream.on('data', (chunk)=>{
-  writeStream.write(chunk)
-})
+// const readStream = fs.createReadStream('./exmaple.txt', 'utf8')
+const writeStream = fs.createReadStream('exmaple.txt')
+const writeStream2 = fs.createWriteStream('exampleWrite2.txt')
+writeStream.pipe(writeStream2);
+
+//piping is actually better.
+
